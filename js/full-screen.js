4e2bf0aa7cb2df58e69464d4f10c07803e6bc725
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-05-07 11:21:38 -0400

    re-wrote fullscreen script to be 75% shorter

--- js/full-screen.js
+++ js/full-screen.js
@@ -1,39 +1,9 @@
 (function() {
-
-	// Declaring carimages so they can be used
-	var websiteImages = document.querySelector(".websiteImages");
-	// Declaring the variable car images onclicked
-	websiteImages.addEventListener("click", function(e) {
-		// Gets the source of the click
-		if(e.target.tagName === "IMG") {
-			// Creates a new element, which is a div/span and assigns it to a variable
-			var overlay = document.createElement("div");
-			var closeButton = document.createElement("span");
-			// Assigns id to newly created div
-			overlay.id = "overlay";
-			closeButton.id = "closeButt"
-			// Append overlay to body
-			document.body.appendChild(overlay);
-			document.body.appendChild(closeButton);
-			// Get image source
-			var imageSource = e.target.src;
-			// Create image
-			var popUpImage = document.createElement("img");
-			popUpImage.id = "popimage";
-			popUpImage.src = imageSource;
-			// Append image to the overlay
-			closeButton.appendChild(popUpImage);
-			overlay.appendChild(popUpImage);
-			popUpImage.addEventListener('click', function() {
-				if(overlay) {
-					overlay.parentElement.removeChild(overlay);
-					closeButton.parentElement.removeChild(closeButton);
-				}
-			})
-			closeButton.addEventListener('click', function() {
-					overlay.parentElement.removeChild(overlay);
-					closeButton.parentElement.removeChild(closeButton);
-			})
+	$('.websiteImages').click(function(e) {
+		if (e.target.tagName === 'IMG') {
+			$('body').append('<div id="overlay"></div>');
+			$('#overlay').append('<img id="popimage" src="' + e.target.src + '"></div>');
+			$('#overlay').click(function() { document.body.removeChild(overlay); })
 		}
 	});
 }());
